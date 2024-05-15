a5232a51172faf256c442dafa61f0dde28b13931
Manoj Kumar <manoj8894421801@gmail.com>
2024-05-15 12:51:44 +0530

    Fix: changed logo name to adminLogo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 
 import './Navbar.css'
 import { assets } from '../../assets/index';
-const { logo } = assets.images;
+const { adminLogo } = assets.images;
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img className='logo' src={logo} alt="" />
+      <img className='logo' src={adminLogo} alt="" />
       <h1>Admin Service</h1>
     </div>
   )
