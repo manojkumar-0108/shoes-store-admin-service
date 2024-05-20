2a4f5a6d53768d2460c1025fbd7479206404355c
Manoj Kumar <manoj8894421801@gmail.com>
2024-05-20 10:43:14 +0530

    Fix: added backend https url

--- src/assets/index.js
+++ src/assets/index.js
@@ -12,4 +12,4 @@ export const API_END_POINTS = {
     ORDERS: '/api/admin/orders/'
 }
 
-export const BACKEND_BASE_URL = 'http://3.90.217.214:4000/'
+export const BACKEND_BASE_URL = 'https://ec2-15-206-128-33.ap-south-1.compute.amazonaws.com/'
